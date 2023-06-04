9cccee2557fd67f6f1220488b1257f1094ffc2a8
liuyue <1308972436@qq.com>
2023-06-04 14:34:30 +0800

    chore: eslint

--- packages/reactivity/__tests__/effect.spec.ts
+++ packages/reactivity/__tests__/effect.spec.ts
@@ -1,16 +1,16 @@
-import { expect, it, describe } from "vitest";
-import { reactive, effect } from "../src/index";
+import { describe, expect, it } from 'vitest'
+import { effect, reactive } from '../src/index'
 
-describe("effect", () => {
-  it("happy path", () => {
-    const original = reactive({ foo: 1 });
-    let number: any;
+describe('effect', () => {
+  it('happy path', () => {
+    const original = reactive({ foo: 1 })
+    let number: any
     effect(() => {
-      number = original.foo + 1;
-    });
-    //effect should exectue once and bar.foo should be 1
-    expect(number).toBe(2);
-    original.foo++;
+      number = original.foo + 1
+    })
+    // effect should exectue once and bar.foo should be 1
+    expect(number).toBe(2)
+    original.foo++
     // expect(bar.foo).toBe(2);
-  });
-});
+  })
+})
--- packages/reactivity/src/index.ts
+++ packages/reactivity/src/index.ts
@@ -1,2 +1,2 @@
-export { reactive } from "./reactive";
-export { effect } from "./effect";
+export { reactive } from './reactive'
+export { effect } from './effect'
