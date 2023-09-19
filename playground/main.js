9af0ce036ff24150fb926090ffc241b63bf2df83
liuyue <1308972436@qq.com>
2023-09-19 23:59:46 +0800

    feat: playground的html引入umd格式文件

--- playground/main.js
+++ playground/main.js
@@ -1,12 +1,12 @@
-const App = {
-  setup() {
-    return {
-      message: 'phil',
-    }
-  },
-  render() {
-    return h('div', `hello${this.message}`)
-  },
-}
+// const App = {
+//   setup() {
+//     return {
+//       message: 'phil',
+//     }
+//   },
+//   render() {
+//     return h('div', `hello${this.message}`)
+//   },
+// }
 
-createApp(App).mount('#app')
+// createApp(App).mount('#app')
